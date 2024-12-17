9972c9108be1f525f78f16701601af2c02f47330
Daniel Meyer <dmeyer@shopware.com>
2024-12-17 15:39:35 +0100

    NEXT-39675 - add todos

--- tests/acceptance/tests/Settings/FlowBuilder.spec.ts
+++ tests/acceptance/tests/Settings/FlowBuilder.spec.ts
@@ -7,32 +7,37 @@ test('As an admin, I want to create new flows from templates, so that I can easi
     AdminFlowBuilderTemplates,
     AdminFlowBuilderCreate,
     AdminFlowBuilderListing,
+    IdProvider,
 
 }) => {
+
+
+    const flowId = IdProvider.getIdPair().uuid;
+    const flowNameUnique = 'Test flow - ' + flowId;
+
     // GIVEN there are Flow Templates
-    // go to templates tab
     await ShopAdmin.goesTo(AdminFlowBuilderTemplates.url());
-    await AdminFlowBuilderTemplates.page.locator('.sw-search-bar').getByPlaceholder('Search flows...').fill('placed');
+    await AdminFlowBuilderTemplates.searchBar.fill('placed');
 
     //WHEN the admin user chooses to create a new flow from a template
-    // create new flow from template
     await AdminFlowBuilderTemplates.page.locator('.sw-data-grid__row').filter({hasText: 'Order placed'}).getByRole('link').getByTestId('sw-icon__regular-long-arrow-right').click();
-    // todo: change name to sth unique
+    await AdminFlowBuilderCreate.nameField.fill(flowNameUnique)
     await AdminFlowBuilderCreate.page.locator('.sw-tabs__content').locator('.sw-flow-detail__tab-flow').click();
-    // assert active flow's structure
-    await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-detail-flow__trigger-card').getByPlaceholder('Select event...')).toBeVisible();
+    // todo: check for content of trigger input field
+    await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-detail-flow__trigger-card').getByRole('textbox')).toBeVisible();
+    await AdminFlowBuilderCreate.page.locator('.sw-flow-detail-flow__trigger-card').getByRole('textbox').click();
     // todo: check that send email action is there
     // todo: save assertions to check again later
     await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-sequence-action__content').locator('.sw-single-select__selection')).toBeVisible();
-    // save flow
+    await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-sequence-action__content').getByRole('button').first()).toContainText('Template: Order confirmation');
     await AdminFlowBuilderCreate.page.locator('.smart-bar__content').locator('.sw-button--primary').getByText('Save').click();
 
     //THEN the new flow will be saved and has exactly the same structure as the template
-    // go to flow builder
     await ShopAdmin.goesTo(AdminFlowBuilderListing.url());
-    await AdminFlowBuilderListing.page.locator('.sw-search-bar').getByPlaceholder('Search flows...').fill('placed');
+    await AdminFlowBuilderListing.page.locator('.sw-search-bar').getByPlaceholder('Search flows...').fill(flowNameUnique);
     // assert one active and one inactive flow
     // todo: only assert new flow (by new unique name and inactive)
+    //await ShopAdmin.expects(AdminFlowBuilderListing.page.locator('.sw-data-grid__row').filter({hasText: 'Order placed'}).getByTestId('sw-icon__regular-checkmark-xs')).toBeVisible();
     await ShopAdmin.expects(AdminFlowBuilderListing.page.locator('.sw-data-grid__row').filter({hasText: 'Order placed'}).getByTestId('sw-icon__regular-checkmark-xs')).toBeVisible();
     await ShopAdmin.expects(AdminFlowBuilderListing.page.locator('.sw-data-grid__row').filter({hasText: 'Order placed'}).getByTestId('sw-icon__regular-times-s')).toBeVisible();
     await AdminFlowBuilderListing.page.locator('.sw-data-grid__row').filter({hasText: 'Order placed'}).getByTestId('sw-icon__regular-times-s').getByRole('link').click();
