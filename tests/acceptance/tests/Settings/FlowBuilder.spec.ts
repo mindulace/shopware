32d9e8b94036d3f7cc753b528df2ed4bca5903f4
Daniel Meyer <d.meyer@shopware.com>
2024-12-20 12:29:28 +0100

    add wait points and finalise abstractions

--- tests/acceptance/tests/Settings/FlowBuilder.spec.ts
+++ tests/acceptance/tests/Settings/FlowBuilder.spec.ts
@@ -13,44 +13,59 @@ test('As an admin, I want to create new flows from templates, so that I can easi
 
 
     const flowId = IdProvider.getIdPair().uuid;
-    const flowNameUnique = 'Test flow - ' + flowId;
+    const flowName = 'Test flow - ' + flowId;
 
     // GIVEN there are Flow Templates
     await ShopAdmin.goesTo(AdminFlowBuilderTemplates.url());
     await AdminFlowBuilderTemplates.searchBar.fill('placed');
+
     //WHEN the admin user chooses to create a new flow from a template
     const adminFlowBuilderTemplatesRow = await AdminFlowBuilderTemplates.getLineItemByFlowName('Order placed')
     await adminFlowBuilderTemplatesRow.createFlowLink.click();
-    await AdminFlowBuilderCreate.nameField.fill(flowNameUnique)
+    await ShopAdmin.expects(AdminFlowBuilderCreate.smartBarHeader).toContainText('Order placed')
+    await AdminFlowBuilderCreate.nameField.fill(flowName)
     await AdminFlowBuilderCreate.flowTab.click();
-    // todo: check for content of trigger input field
+    // todo: make task
     await ShopAdmin.expects(AdminFlowBuilderCreate.triggerSelectField).toBeVisible();
-    await AdminFlowBuilderCreate.triggerSelectField.click();
     await AdminFlowBuilderCreate.triggerSelectField.hover();
-    const tooltip = await AdminFlowBuilderCreate.page.waitForSelector('.sw-tooltip');
-    const tooltipText = await tooltip.innerText(); // Get text from the tooltip
-    //console.log(await AdminFlowBuilderCreate.triggerSelectField.innerText());
-    // todo: check that send email action is there
-    // todo: save assertions to check again later - compare template and resulting flow -
-    // assert active flow's structure
+    let tooltip = await AdminFlowBuilderCreate.page.waitForSelector('.sw-tooltip');
+    let tooltipText = await tooltip.innerText(); // Get text from the tooltip
+    await ShopAdmin.expects(tooltipText).toEqual('Checkout / Order / Placed');
+    // todo: (optional) save assertions to check again later - compare template and resulting flow - task?
+    // implicitly asserts there is no other action
     await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-sequence-action__content').locator('.sw-single-select__selection')).toBeVisible();
+    // asserts there are no other containers
+    await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-sequence-condition__container')).not.toBeVisible();
+    await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-delay-action__delay_card')).not.toBeVisible();
+    // asserts email action (only partially available during test)
+    await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-sequence-action__content').getByRole('button').first()).toContainText('Template: Order confirmation');
     await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-sequence-action__content').getByRole('button').first()).toContainText('Template: Order confirmation');
     await AdminFlowBuilderCreate.saveButton.click();
-    // eslint-disable-next-line playwright/no-networkidle
-    await AdminFlowBuilderCreate.page.waitForLoadState('networkidle')
+
     //THEN the new flow will be saved and has exactly the same structure as the template
     await ShopAdmin.goesTo(AdminFlowBuilderListing.url());
-    // note: fill opens dropdown ...
-    await AdminFlowBuilderListing.searchBar.pressSequentially(flowNameUnique);
-    //await AdminFlowBuilderListing.page.locator('.sw-tabs__content').getByTitle('My flows').click();
-    await AdminFlowBuilderListing.page.locator('.sw-card-view__content').click();
-    const adminFlowBuilderListingRow = await AdminFlowBuilderListing.getLineItemByFlowName(flowNameUnique)
-    await AdminFlowBuilderListing.page.waitForTimeout(2000);
-    await ShopAdmin.expects(adminFlowBuilderListingRow.flowDisabledCheckmark).toBeVisible();
-    await adminFlowBuilderListingRow.flowNameText.click();
-    await ShopAdmin.expects(AdminFlowBuilderDetail.nameField).toHaveValue(flowNameUnique);
+    // fill opens dropdown
+    await AdminFlowBuilderListing.searchBar.fill(flowName);
+    // wait for and remove dropdown
+    await ShopAdmin.expects(AdminFlowBuilderListing.searchDropdown).toBeVisible();
+    await AdminFlowBuilderListing.pageBackground.click({position: {x: 25, y: 255}});
+    await ShopAdmin.expects(AdminFlowBuilderListing.searchDropdown).not.toBeVisible();
+    const listingRow = await AdminFlowBuilderListing.getLineItemByFlowName(flowName)
+    await ShopAdmin.expects(listingRow.flowDisabledCheckmark).toBeVisible();
+    await listingRow.flowNameText.click();
+    await ShopAdmin.expects(AdminFlowBuilderDetail.nameField).toHaveValue(flowName);
     await AdminFlowBuilderDetail.flowTab.click();
-    // assert inactive flow's structure
-    await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-detail-flow__trigger-card').getByPlaceholder('Select event...')).toBeVisible();
-    await ShopAdmin.expects(AdminFlowBuilderCreate.page.locator('.sw-flow-sequence-action__content').locator('.sw-single-select__selection')).toBeVisible();
+    // todo: make task
+    await ShopAdmin.expects(AdminFlowBuilderDetail.triggerSelectField).toBeVisible();
+    await AdminFlowBuilderDetail.triggerSelectField.hover();
+    tooltip = await AdminFlowBuilderDetail.page.waitForSelector('.sw-tooltip');
+    tooltipText = await tooltip.innerText(); // Get text from the tooltip
+    await ShopAdmin.expects(tooltipText).toEqual('Checkout / Order / Placed');
+    await ShopAdmin.expects(AdminFlowBuilderDetail.page.locator('.sw-flow-sequence-action__content').locator('.sw-single-select__selection')).toBeVisible();
+    // asserts there are no other containers
+    await ShopAdmin.expects(AdminFlowBuilderDetail.page.locator('.sw-flow-sequence-condition__container')).not.toBeVisible();
+    await ShopAdmin.expects(AdminFlowBuilderDetail.page.locator('.sw-flow-delay-action__delay_card')).not.toBeVisible();
+    // asserts email action
+    await ShopAdmin.expects(AdminFlowBuilderDetail.page.locator('.sw-flow-sequence-action__content').locator('.sw-single-select__selection')).toBeVisible();
+    await ShopAdmin.expects(AdminFlowBuilderDetail.page.locator('.sw-flow-sequence-action__content').getByRole('button').first()).toContainText('Template: Order confirmation');
 });
